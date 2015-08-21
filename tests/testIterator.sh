1a721165986daf50999f65ec5801b01af921834d
Manuel Limones <limonesu.me@gmail.com>
2015-08-21 18:50:09 -0500

    Change display format for test scrip

--- tests/testIterator.sh
+++ tests/testIterator.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 for testDir in $(ls -d test_*); do 
-	(cd $testDir; make test | grep "FAILED" && make test || echo $testDir ok); 
+	(cd $testDir; make test | grep "FAILED" && make test || echo "$testDir -> OK"); 
 done
 
